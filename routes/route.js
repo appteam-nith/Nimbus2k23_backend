9a754f05399f2039ab45ab4e71d945abadc386b3
mukesh <mukesh1150935@gmail.com>
2023-02-05 16:35:23 +0530

    post feed added

--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getUsers,postUsers,patchUser, deleteUser,getUsersByName,getUserById} from '../controllers/User-controller.js';
+import { getPost,createPost,updatePost,deletePost,likePost } from '../controllers/post.js';
 
 const router =express.Router(); 
 
@@ -10,5 +11,11 @@ router.post("/users",postUsers);
 
 router.patch("/users/:id",patchUser);
 router.delete("/users/:id",deleteUser);
+
+router.post("/posts",createPost);
+router.get("/posts",getPost);
+router.patch("/posts/:id",updatePost);
+router.delete("posts/:id",deletePost);
+router.patch("/posts/:id/like",likePost);
 
 export default router;
